10f6ee43a8157104b7b5bf3d8a49cf33c80ac79e
tvCu <pet.cu20791@gmail.com>
2023-12-23 02:18:40 +0700

    Cu fix

--- rananu-spring-mvc/src/main/java/vn/rananu/spring/mvc/validation/NullOrNotEmptyValidator.java
+++ rananu-spring-mvc/src/main/java/vn/rananu/spring/mvc/validation/NullOrNotEmptyValidator.java
@@ -4,11 +4,12 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import vn.rananu.spring.mvc.validation.constraints.NullOrNotEmpty;
 
+import java.util.Collection;
 import java.util.List;
 
-public class NullOrNotEmptyValidator implements ConstraintValidator<NullOrNotEmpty, List<?>> {
+public class NullOrNotEmptyValidator implements ConstraintValidator<NullOrNotEmpty, Collection<?>> {
     @Override
-    public boolean isValid(List<?> value, ConstraintValidatorContext context) {
+    public boolean isValid(Collection<?> value, ConstraintValidatorContext context) {
         return value != null && !value.isEmpty();
     }
 }
